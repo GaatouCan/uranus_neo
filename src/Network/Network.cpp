ff194df4e70fd691ddf8906d91b6a06494e887f9
Astria <1752651025@qq.com>
2025-07-24 23:16:35 +0800

    Initial commit

--- src/Network/Network.cpp
+++ src/Network/Network.cpp
@@ -1,14 +1,77 @@
 #include "Network.h"
+#include "Base/Package.h"
 
 #include <string>
+#include <spdlog/spdlog.h>
+
 
 static const std::string key = "1d0fa7879b018b71a00109e7e29eb0c5037617e9eddc3d46e95294fc1fa3ad50";
+
+UNetwork::UNetwork()
+    : mAcceptor(mIOContext),
+      mPackagePool(nullptr) {
+}
+
+UNetwork::~UNetwork() {
+    Stop();
+
+    if (mThread.joinable()) {
+        mThread.join();
+    }
+}
 
 void UNetwork::Initial() {
     if (mState != EModuleState::CREATED)
         return;
 
+    mPackagePool = make_shared<TRecycler<FPackage>>();
+    mPackagePool->Initial();
+
+    mThread = std::thread([this] {
+       // SPDLOG_INFO("Network Work In Thread[{}]", utils::ThreadIDToInt(std::this_thread::get_id()));
+
+       asio::signal_set signals(mIOContext, SIGINT, SIGTERM);
+       signals.async_wait([this](auto, auto) {
+           Stop();
+       });
+       mIOContext.run();
+   });
+
     mState = EModuleState::INITIALIZED;
+}
+
+void UNetwork::Start() {
+    if (mState != EModuleState::INITIALIZED)
+        return;
+
+    uint16_t port = 8080;
+    // if (const auto *config = GetServer()->GetModule<UConfig>(); config != nullptr) {
+    //     port = config->GetServerConfig()["server"]["port"].as<uint16_t>();
+    // }
+
+    co_spawn(mIOContext, WaitForClient(port), detached);
+    mState = EModuleState::RUNNING;
+}
+
+void UNetwork::Stop() {
+    if (mState == EModuleState::STOPPED)
+        return;
+
+    mState = EModuleState::STOPPED;
+
+    if (!mIOContext.stopped()) {
+        mIOContext.stop();
+    }
+
+    mConnectionMap.clear();
+}
+
+io_context &UNetwork::GetIOContext() {
+    return mIOContext;
+}
+
+awaitable<void> UNetwork::WaitForClient(uint16_t port) {
+    co_return;
 }
 
 const unsigned char *UNetwork::GetEncryptionKey() {
@@ -17,7 +80,7 @@ const unsigned char *UNetwork::GetEncryptionKey() {
 }
 
 std::shared_ptr<FPackage> UNetwork::BuildPackage() {
-    return nullptr;
+    return std::dynamic_pointer_cast<FPackage>(mPackagePool->Acquire());
 }
 
 std::vector<uint8_t> UNetwork::HexToBytes(const std::string &hex) {
--- src/Network/Network.h
+++ src/Network/Network.h
@@ -1,31 +1,56 @@
 #pragma once
 
 #include "Module.h"
+#include "Base/Types.h"
+#include "Base/Recycler.h"
 
 #include <memory>
 #include <string>
 #include <vector>
+#include <thread>
+
 
 class FPackage;
+class UConnection;
 
 
 class BASE_API UNetwork final : public IModuleBase {
 
     DECLARE_MODULE(UNetwork)
 
+    io_context mIOContext;
+    ATcpAcceptor mAcceptor;
+
+    std::thread mThread;
+
+    shared_ptr<IRecyclerBase> mPackagePool;
+
+    std::unordered_map<int64_t, shared_ptr<UConnection>> mConnectionMap;
+    mutable std::shared_mutex mMutex;
+
 protected:
+    UNetwork();
+
     void Initial() override;
+    void Start() override;
+    void Stop() override;
 
 public:
+    ~UNetwork() override;
+
     [[nodiscard]] constexpr const char *GetModuleName() const override {
         return "Network";
     }
+
+    [[nodiscard]] io_context &GetIOContext();
 
     const unsigned char* GetEncryptionKey();
 
     std::shared_ptr<FPackage> BuildPackage();
 
 private:
+    awaitable<void> WaitForClient(uint16_t port);
+
     static std::vector<uint8_t> HexToBytes(const std::string &hex);
 };
 
