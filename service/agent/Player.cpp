f0054871cd7ff76a14c4354099c570f0b7de78f8
admin <xxx@xx.xom>
2025-07-30 20:25:13 +0800

    Initial commit

--- service/agent/Player.cpp
+++ service/agent/Player.cpp
@@ -7,3 +7,13 @@ UPlayer::UPlayer() {
 
 UPlayer::~UPlayer() {
 }
+
+extern "C" {
+    SERVICE_API IServiceBase *CreateInstance() {
+        return new UPlayer();
+    }
+
+    SERVICE_API void DestroyInstance(IServiceBase *inst) {
+        delete dynamic_cast<UPlayer *>(inst);
+    }
+}
