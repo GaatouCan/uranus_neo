20d8f2a429f80c0431aace702935d91c1d93999b
admin <xxx@xx.xom>
2025-08-04 16:00:26 +0800

    Initial commit

--- service/agent/component/appear/AppearComponent.cpp
+++ service/agent/component/appear/AppearComponent.cpp
@@ -1,6 +1,59 @@
 #include "AppearComponent.h"
 #include "../../Protocol.h"
 
-void protocol::AppearanceRequest(uint32_t pid, const std::shared_ptr<FPacket> &pkg, UPlayer *plr) {
+#include <bsoncxx/builder/basic/document.hpp>
+#include <bsoncxx/builder/basic/array.hpp>
 
+void protocol::AppearanceRequest(uint32_t pid, const std::shared_ptr<FPacket> &pkg, UPlayer *plr) {
+}
+
+UAppearComponent::UAppearComponent()
+    : mCurrentIndex(0) {
+}
+
+UAppearComponent::~UAppearComponent() {
+}
+
+bsoncxx::document::value UAppearComponent::Serialize() const {
+    bsoncxx::builder::basic::document doc;
+
+    doc.append(bsoncxx::builder::basic::kvp("current_index", mCurrentIndex));
+
+    {
+        bsoncxx::builder::basic::array array_builder;
+        for (const auto &val: mAvatarList) {
+            array_builder.append(
+                bsoncxx::builder::basic::make_document(
+                    bsoncxx::builder::basic::kvp("index", val.index),
+                    bsoncxx::builder::basic::kvp("active", val.active)
+                )
+            );
+        }
+        doc.append(bsoncxx::builder::basic::kvp("avatar_list", array_builder.extract()));
+    }
+
+    return doc.extract();
+}
+
+void UAppearComponent::Deserialize(const bsoncxx::document::value &doc) {
+    if (const auto elem = doc["current_index"]; elem && elem.type() == bsoncxx::type::k_int32) {
+        mCurrentIndex = elem.get_int32().value;
+    }
+
+    if (const auto array_elem = doc["avatar_list"]; array_elem && array_elem.type() == bsoncxx::type::k_array) {
+        for (auto &&val: array_elem.get_array().value) {
+            auto avatar_view = val.get_document().value;
+
+            FAvatarInfo avatar;
+
+            if (auto elem = avatar_view["index"]; elem && elem.type() == bsoncxx::type::k_int32) {
+                avatar.index = elem.get_int32().value;
+            }
+            if (auto elem = avatar_view["active"]; elem && elem.type() == bsoncxx::type::k_bool) {
+                avatar.active = elem.get_bool().value;
+            }
+
+            mAvatarList.push_back(avatar);
+        }
+    }
 }
--- service/agent/component/appear/AppearComponent.h
+++ service/agent/component/appear/AppearComponent.h
@@ -1,9 +1,27 @@
 #pragma once
 
-#include <Gateway/PlayerComponent.h>
+#include "Gateway/PlayerComponent.h"
 
+#include <vector>
+#include <bsoncxx/document/value.hpp>
+
+struct FAvatarInfo {
+    int index;
+    bool active;
+};
 
 class UAppearComponent final : public IPlayerComponent {
 
+
+public:
+    UAppearComponent();
+    ~UAppearComponent() override;
+
+    bsoncxx::document::value Serialize() const;
+    void Deserialize(const bsoncxx::document::value& doc);
+
+private:
+    int mCurrentIndex;
+    std::vector<FAvatarInfo> mAvatarList;
 };
 
