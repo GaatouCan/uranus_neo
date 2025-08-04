28935f6bcdbcc444a901e45d1b0e41c87f0f91b5
admin <xxx@xx.xom>
2025-08-04 17:14:36 +0800

    Initial commit

--- service/agent/component/appear/AppearComponent.cpp
+++ service/agent/component/appear/AppearComponent.cpp
@@ -9,51 +9,23 @@ void protocol::AppearanceRequest(uint32_t pid, const std::shared_ptr<FPacket> &p
 
 UAppearComponent::UAppearComponent()
     : mCurrentIndex(0) {
+
 }
 
 UAppearComponent::~UAppearComponent() {
 }
 
-bsoncxx::document::value UAppearComponent::Serialize() const {
-    bsoncxx::builder::basic::document doc;
+DB_SERIALIZE_BEGIN(UAppearComponent)
+    DB_WRITE_FIELD("current_index", mCurrentIndex);
+    DB_WRITE_ARRAY("avatar_list", mAvatarList,
+        DB_WRITE_ARRAY_ELEMENT("index", index),
+        DB_WRITE_ARRAY_ELEMENT("active", active))
+DB_SERIALIZE_END
 
-    doc.append(bsoncxx::builder::basic::kvp("current_index", mCurrentIndex));
-
-    {
-        bsoncxx::builder::basic::array array_builder;
-        for (const auto &val: mAvatarList) {
-            array_builder.append(
-                bsoncxx::builder::basic::make_document(
-                    bsoncxx::builder::basic::kvp("index", val.index),
-                    bsoncxx::builder::basic::kvp("active", val.active)
-                )
-            );
-        }
-        doc.append(bsoncxx::builder::basic::kvp("avatar_list", array_builder.extract()));
-    }
-
-    return doc.extract();
-}
-
-void UAppearComponent::Deserialize(const bsoncxx::document::value &doc) {
-    if (const auto elem = doc["current_index"]; elem && elem.type() == bsoncxx::type::k_int32) {
-        mCurrentIndex = elem.get_int32().value;
-    }
-
-    if (const auto array_elem = doc["avatar_list"]; array_elem && array_elem.type() == bsoncxx::type::k_array) {
-        for (auto &&val: array_elem.get_array().value) {
-            auto avatar_view = val.get_document().value;
-
-            FAvatarInfo avatar;
-
-            if (auto elem = avatar_view["index"]; elem && elem.type() == bsoncxx::type::k_int32) {
-                avatar.index = elem.get_int32().value;
-            }
-            if (auto elem = avatar_view["active"]; elem && elem.type() == bsoncxx::type::k_bool) {
-                avatar.active = elem.get_bool().value;
-            }
-
-            mAvatarList.push_back(avatar);
-        }
-    }
-}
+DB_DESERIALIZE_BEGIN(UAppearComponent)
+    DB_READ_FIELD("current_index", int32, mCurrentIndex)
+    DB_READ_ARRAY("avatar_list", mAvatarList,
+        DB_READ_ARRAY_ELEMENT("index", int32, index)
+        DB_READ_ARRAY_ELEMENT("active", int32, active)
+    )
+DB_DESERIALIZE_END
