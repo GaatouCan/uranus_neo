df9d5ebd692ca74f75481dc1ad070fd7fd54717d
Astria <1752651025@qq.com>
2025-08-16 02:04:36 +0800

    Initial commit

--- src/base/SharedLibrary.h
+++ src/base/SharedLibrary.h
@@ -7,7 +7,7 @@
 #include <filesystem>
 #if defined(_WIN32) || defined(_WIN64)
 #define WIN32_LEAN_AND_MEAN
-#include <Windows.h>
+#include <windows.h>
 using AModuleHandle = HMODULE;
 #else
 #include <dlfcn.h>
